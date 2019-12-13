2f16146ce77a8b90ad1ee8a9b8c26f03516d228e
Jake <jprouse2@uwo.ca>
2019-12-12 22:28:44 -0500

    changed url back

--- src/App.js
+++ src/App.js
@@ -2,6 +2,47 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/** 
+ * Dynamic particle effects
+ */
+function header() {
+
+}
+
+/** 
+ * Image of my face
+ */
+function myFace() {
+
+}
+
+/**
+ * The about me section
+ */
+function aboutSection() {
+
+}
+
+/**
+ * A project section has multiple projects that contain
+ * - a title
+ * - an image
+ * - a description
+ */
+function project() {
+
+}
+
+/**
+ * This section contains all the projects I've done, made up of projects
+ */
+function projectSection() {
+
+}
+
+/**
+ * Main app
+ */
 function App() {
   return (
     <div className="App">
