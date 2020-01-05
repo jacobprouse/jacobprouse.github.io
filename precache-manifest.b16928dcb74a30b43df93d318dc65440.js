d5faedf84de7d36209cf660ddbf3064671f4f820
Jake <jprouse2@uwo.ca>
2020-01-04 20:11:27 -0500

    Updates

---
+++ precache-manifest.b16928dcb74a30b43df93d318dc65440.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fd13c85c4649921db5a73b1a0ff2933",
+    "url": "/personal/index.html"
+  },
+  {
+    "revision": "5af941f14bd20014d12c",
+    "url": "/personal/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "b291a36a31e02aff9158",
+    "url": "/personal/static/css/main.8cdbe6a6.chunk.css"
+  },
+  {
+    "revision": "5af941f14bd20014d12c",
+    "url": "/personal/static/js/2.640081dc.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/personal/static/js/2.640081dc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b291a36a31e02aff9158",
+    "url": "/personal/static/js/main.657abdb6.chunk.js"
+  },
+  {
+    "revision": "76117dc308d8b8ced3e6",
+    "url": "/personal/static/js/runtime-main.6e067d50.js"
+  },
+  {
+    "revision": "87321b02c27fc57f4be137ddcf1586b5",
+    "url": "/personal/static/media/placeholder.87321b02.png"
+  },
+  {
+    "revision": "4411fe4a47dd0772bd959652f5b45c34",
+    "url": "/personal/static/media/website.4411fe4a.png"
+  }
+]);
