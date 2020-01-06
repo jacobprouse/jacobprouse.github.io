0a87dacde966da53d595e301b79fa1b5fcf246db
Jake <jprouse2@uwo.ca>
2020-01-06 15:14:51 -0500

    Revert "changes to package.json"
    This reverts commit 7d96c8c6fb332e7cbc90003fd009ebefd3c78514.

--- src/components/about.js
+++ src/components/about.js
@@ -33,7 +33,7 @@ class About extends React.Component {
               </div>
             </Col>
             <Col>
-              <Face href="https://jacobprouse.github.io"></Face>
+              <Face href="https://jacobprouse.github.io/personal"></Face>
             </Col>
           </Row>
         </div>
