07bbc7d7185992c9be8c7a63e69ddf6d62cb2e67
Jake <jprouse2@uwo.ca>
2020-01-08 16:19:59 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b5b79e997c73531742688238f81a0967.js"
+  "/precache-manifest.4420d7a40961c1f4d5c7bb6efc786a29.js"
 );
 
 self.addEventListener('message', (event) => {
