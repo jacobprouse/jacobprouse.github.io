bfc0536905de691d01ae350eb7cf029ebe5c118d
Jake <jprouse2@uwo.ca>
2020-01-17 16:31:53 -0500

    Updates

--- precache-manifest.3281c515afacb6e1588e1e69737b65eb.js
+++ precache-manifest.3281c515afacb6e1588e1e69737b65eb.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b561db1b13a72e5b30400af2c5186cb6",
+    "revision": "0e356b1c35a1ef5d6e93dd8e0119d90a",
     "url": "/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.17e5ed98.chunk.css"
   },
   {
-    "revision": "529aada58085de7d9905",
-    "url": "/static/css/main.e383f393.chunk.css"
+    "revision": "3085dd7c24c91a17a4c8",
+    "url": "/static/css/main.d568bc53.chunk.css"
   },
   {
     "revision": "ac28091f9b2cd219ab61",
@@ -20,16 +20,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.5c9de7c9.chunk.js.LICENSE"
   },
   {
-    "revision": "529aada58085de7d9905",
-    "url": "/static/js/main.5c0aa1bb.chunk.js"
+    "revision": "3085dd7c24c91a17a4c8",
+    "url": "/static/js/main.2546078b.chunk.js"
   },
   {
     "revision": "798d9f1c493afa821278",
     "url": "/static/js/runtime-main.3d8610f7.js"
   },
   {
-    "revision": "5f7ade43e6af247e545294f494f2ae69",
-    "url": "/static/media/face.5f7ade43.png"
+    "revision": "67289d2947abd8c60f0a77a6b9d0b37e",
+    "url": "/static/media/profile.67289d29.png"
   },
   {
     "revision": "4411fe4a47dd0772bd959652f5b45c34",
